18aa1eb47839dc169a9c7ccd3723e2c283657bcf
walters-skn <mickywalters72@gmail.com>
2022-03-31 15:31:03 -0500

    Title, description and image added

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,14 +1,38 @@
 <template>
     <ul class="news__list">
-      <li class="news__item">News item 1</li>
-      <li class="news__item">News item 2</li>
-      <li class="news__item">News item 3</li>
+      <li v-for="article in articles" class="news__item">
+      {{ article.title }}, {{article.description}} 
+      <img :src="article.urlToImage"/>
+      </li>
+
     </ul>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
-}, };
+    return {
+        articles: []
+    };
+    },
+    created() {
+
+        let self = this;
+
+        fetch('https://newsapi.org/v2/top-headlines?country=us',
+        {
+            headers:
+            {
+                Authorization: `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`           }
+        })
+        .then(function(response){
+            return response.json();
+        })
+        
+        .then(function(data) {
+            console.log(data);
+            self.articles = data.articles;
+        });
+    }
+};
 </script>
